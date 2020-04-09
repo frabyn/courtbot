69c2e682a5c9dece185a9a5b6d5cb91ab509a579
Franklin Bynum <franklin@bynumlaw.net>
2020-04-09 22:00:12 +0000

    Update settings.py

--- courtbot/settings.py
+++ courtbot/settings.py
@@ -78,11 +78,6 @@ WSGI_APPLICATION = 'courtbot.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': os.getenv('PGSQL_USER'),
-        'PASSWORD': os.getenv('PGSQL_SECRET'),
-        'HOST': os.getenv('PGSQL_SERVER'),
-        'PORT': '5432',
     }
 }
 
