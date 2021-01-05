e65cada84d62231d1686aca7ab3f22aa63917e30
Franklin Bynum <franklin@mini.local>
2021-01-05 18:59:39 +0000

    Bump up max length of model fields

--- case_manager/models.py
+++ case_manager/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Case(models.Model):
     court = models.CharField(max_length=3)
-    defendant_name = models.CharField(max_length=25, blank=False)
+    defendant_name = models.CharField(max_length=30, blank=False)
     cause_number = models.CharField(max_length=7, primary_key=True)
     days_old = models.CharField(max_length=15)
     total_settings = models.CharField(max_length=3, blank=True)
@@ -11,9 +11,9 @@ class Case(models.Model):
     bail = models.CharField(max_length=10)
     bond_type = models.CharField(max_length=1, blank=True)
     next_setting_type = models.CharField(max_length=4, blank=False)
-    defense_lawyer = models.CharField(max_length=25, blank=True)
+    defense_lawyer = models.CharField(max_length=30, blank=True)
     next_setting = models.DateField()
-    charge = models.CharField(max_length=25, blank=False)
+    charge = models.CharField(max_length=30, blank=False)
     felony_pending = models.BooleanField(blank=True, null=True)
     last_updated = models.DateField(auto_now=True)
     
