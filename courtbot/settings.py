cabf9590c490c1308a9c32edbf5569a1c5dbc8c4
Franklin Bynum <franklin@bynumlaw.net>
2021-04-08 00:22:28 -0500

    Debug off by default

--- courtbot/settings.py
+++ courtbot/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = ['frabyn.pythonanywhere.com',
                  'localhost', '127.0.0.1']
